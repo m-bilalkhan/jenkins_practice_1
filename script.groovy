0320d6ac695d5e17df70a9f141a54dc2b479cc83
m-bilal-khan <bilalkhan.192003@gmail.com>
2024-10-06 21:25:23 +0500

    minor changes

--- script.groovy
+++ script.groovy
@@ -1,5 +1,5 @@
 def buildApplication(){
-  withCredentials([usernamePassword(credentialsId: '', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-repo-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
     sh 'docker build -t mbilalkhan/practice-repo:1.0.0 .'
     sh 'echo $PASS | docker login -u $USER --password-stdin'
     sh 'docker push mbilalkhan/practice-repo:1.0.0'
